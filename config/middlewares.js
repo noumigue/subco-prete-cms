9ddba44555e1993b7f4f47692ee06d3ee19c0076
snoumigue <noumigue@neema.co.za>
2026-05-09 02:58:17 +0100

    Configure DigitalOcean Spaces uploads

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,33 +1,69 @@
-module.exports = [
-  "strapi::logger",
-  "strapi::errors",
-  "strapi::security",
-  {
-    name: "strapi::cors",
-    config: {
-      origin: [
-        "https://prete-suco-portal.onrender.com",
-        "http://localhost:3000"
-      ],
-      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
-      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
-      keepHeaderOnError: true
-    }
-  },
-  {
-    name: "strapi::body",
-    config: {
-      formLimit: "10mb",
-      jsonLimit: "10mb",
-      textLimit: "10mb",
-      formidable: {
-        maxFileSize: 10 * 1024 * 1024
+const normalizeOrigin = (value) => {
+  if (!value) return null;
+
+  try {
+    return new URL(value).origin;
+  } catch {
+    return value.replace(/\/+$/, '');
+  }
+};
+
+module.exports = ({ env }) => {
+  const corsOrigins = env.array('CORS_ORIGINS', [
+    "https://prete-suco-portal.onrender.com",
+    "http://localhost:3000"
+  ]);
+
+  const spacesOrigins = [
+    env('DO_SPACES_BASE_URL'),
+    env('DO_SPACES_CDN_URL'),
+    env('DO_SPACES_BUCKET') && env('DO_SPACES_REGION')
+      ? `https://${env('DO_SPACES_BUCKET')}.${env('DO_SPACES_REGION')}.digitaloceanspaces.com`
+      : null,
+    env('DO_SPACES_REGION') ? `https://${env('DO_SPACES_REGION')}.digitaloceanspaces.com` : null,
+  ].map(normalizeOrigin).filter(Boolean);
+
+  return [
+    "strapi::logger",
+    "strapi::errors",
+    {
+      name: "strapi::security",
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            "connect-src": ["'self'", "https:"],
+            "img-src": ["'self'", "data:", "blob:", "market-assets.strapi.io", ...spacesOrigins],
+            "media-src": ["'self'", "data:", "blob:", ...spacesOrigins],
+            upgradeInsecureRequests: null,
+          },
+        },
+      },
+    },
+    {
+      name: "strapi::cors",
+      config: {
+        origin: corsOrigins,
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+        headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+        keepHeaderOnError: true
       }
-    }
-  },
-  "strapi::poweredBy",
-  "strapi::query",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public"
-];
+    },
+    {
+      name: "strapi::body",
+      config: {
+        formLimit: "10mb",
+        jsonLimit: "10mb",
+        textLimit: "10mb",
+        formidable: {
+          maxFileSize: 10 * 1024 * 1024
+        }
+      }
+    },
+    "strapi::poweredBy",
+    "strapi::query",
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public"
+  ];
+};
--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,43 @@
-module.exports = () => ({});
+module.exports = ({ env }) => {
+  const bucket = env('DO_SPACES_BUCKET');
+  const region = env('DO_SPACES_REGION');
+  const accessKeyId = env('DO_SPACES_KEY');
+  const secretAccessKey = env('DO_SPACES_SECRET');
+
+  if (!bucket || !region || !accessKeyId || !secretAccessKey) {
+    return {};
+  }
+
+  const endpoint = env('DO_SPACES_ENDPOINT', `https://${region}.digitaloceanspaces.com`);
+  const baseUrl = env('DO_SPACES_BASE_URL', `https://${bucket}.${region}.digitaloceanspaces.com`);
+
+  return {
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          baseUrl,
+          rootPath: env('DO_SPACES_ROOT_PATH', 'uploads'),
+          s3Options: {
+            credentials: {
+              accessKeyId,
+              secretAccessKey,
+            },
+            region,
+            endpoint,
+            forcePathStyle: env.bool('DO_SPACES_FORCE_PATH_STYLE', false),
+            params: {
+              ACL: env('DO_SPACES_ACL', 'public-read'),
+              Bucket: bucket,
+            },
+          },
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
+  };
+};
